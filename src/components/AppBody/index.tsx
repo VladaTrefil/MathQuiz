ad83127c812b500a86c4f44d1167a4549ae3c9cb
Vlada <vladislav.trefil@protonmail.com>
2022-06-04 20:34:03 +0200

    cleanup

---
+++ src/components/AppBody/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+function AppBody() {
+  return (
+    <Container>
+      <Box sx={{ p: 2 }}>
+        <Button>Click this</Button>
+      </Box>
+    </Container>
+  )
+}
+
+export default AppBody
---
+++ src/components/Theme.tsx
@@ -0,0 +1,24 @@
+import { ThemeProvider, createTheme } from '@mui/material'
+import App from './App'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import 'normalize.css'
+
+const theme = createTheme({
+  palette: {},
+  typography: {}
+})
+
+function Theme() {
+  return (
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+  )
+}
+
+export default Theme
